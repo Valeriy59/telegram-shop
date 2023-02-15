1401bbffc4dd5c1c174ff31f858a4f940087d29f
Valeriy Litvinov <valitvinoff@mail.ru>
2023-02-15 20:53:27 +0300

    feat: Header into App

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Button.module.css'
+
 export const Button = (props) => {
     return (
         <button {...props} className={s.button + props.className}/>
